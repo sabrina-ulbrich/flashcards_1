470568bf37a504267e86c52330f01d8d3db472f1
sabrina-ulbrich <su@evolos.de>
2013-08-27 02:03:08 +0200

    Finish first layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Flashcards1::Application.routes.draw do
-  get "static_pages/home"
+  get "users/new"
+
+  root 'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get'
+  # get "static_pages/home"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,16 +2,14 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+	subject { page }
+
   describe "Home page" do
+  	# visit the root path before each example
+  	before { visit root_path }
 
-    it "should have the content 'Flashcards'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Flashcards')
-    end
-
-    it "should have the title 'Flashcards'" do
-    	visit '/static_pages/home'
-    	expect(page).to have_title("Flashcards")
-    end
+    it { should have_content('Flashcards') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
   end
 end
