7c495b53aff413aea2ad653ed9d7fa441a36e64f
sabrina-ulbrich <su@evolos.de>
2013-08-27 13:04:21 +0200

    title for signup-page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -6,6 +6,6 @@ describe "UserPages" do
   	before { visit signup_path }
 
     it { should have_content('Sign up') }
-    #it { should have_title("Sign up") }
+    it { should have_title("Sign up") }
   end
 end
