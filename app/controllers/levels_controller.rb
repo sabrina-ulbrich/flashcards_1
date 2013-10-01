c84058eb7e5b68528dbeee77ff1ee93c07d62f6d
sabrina-ulbrich <su@evolos.de>
2013-10-01 23:45:35 +0200

    changed level 0 only for new cards

--- app/controllers/levels_controller.rb
+++ app/controllers/levels_controller.rb
@@ -9,25 +9,13 @@ class LevelsController < ApplicationController
   def create
     card = Card.find(params[:card_id])
     cardset = card.cardset
-
-    if params[:commit] == "correct"
-      flash[:success] = "The card raises its level!"
-      status = 1
-    elsif params[:commit] == "false"
-      flash[:notice] = "The card stays on the same level."
-      status = 0
-    end
-
+    status = params[:commit] == 'correct' ? 2 : 1
     sort_order = cardset.max_order(status, current_user.id) + 1
     card.levels.create(:status => status, :user_id => current_user.id, :sort_order => sort_order)
     
     next_card = cardset.cards.with_levels_for(current_user.id).with_status(0).first
-    level = next_card.level_for_user(current_user)
-
-    if next_card and !level
+    if next_card
       redirect_to new_cardset_card_level_path(cardset, next_card)
-    elsif next_card
-      redirect_to edit_cardset_card_level_path(cardset, next_card, level)
     else
       redirect_to cardset_path(cardset)
     end
@@ -41,21 +29,15 @@ class LevelsController < ApplicationController
 
   def update
     cardset = Cardset.find(params[:cardset_id])
-    @level = Level.find(params[:id])
+    level = Level.find(params[:id])    
+    current_status = level.status
+    level.status = params[:commit] == 'correct' ? level.status + 1 : 1
+    level.sort_order = cardset.max_order(level.status, current_user.id) + 1
+    level.save
     
-    current_status = @level.status
-    if params[:commit] == "correct"
-      @level.status += 1
-    elsif params[:commit] == "false"
-      @level.status = 0
-    end
-    
-    @level.sort_order = cardset.max_order(@level.status, current_user.id) + 1
-    @level.save
-    
-    @card = cardset.cards.with_levels_for(current_user.id).with_status(current_status).first
-    if @card
-      redirect_to edit_cardset_card_level_path(cardset, @card, @card.level_for_user(current_user))
+    next_card = cardset.cards.with_levels_for(current_user.id).with_status(current_status).first
+    if next_card
+      redirect_to edit_cardset_card_level_path(cardset, next_card, next_card.level_for_user(current_user))
     else
       redirect_to cardset_path(cardset)
     end
--- spec/requests/learning_pages_spec.rb
+++ spec/requests/learning_pages_spec.rb
@@ -5,7 +5,7 @@ describe "LearningPages" do
   let(:cardset) { FactoryGirl.create(:cardset) }
 
   let(:cards) do
-    (1..5).to_a.map do | num |
+    (1..6).to_a.map do | num |
       cardset.cards.create!(question: "question #{num}", answer: "answer #{num}")
     end
   end
@@ -15,20 +15,20 @@ describe "LearningPages" do
   describe "Correct card order" do
     let!(:level_1) { cards.first.levels.create!(user: user, status: 2, sort_order: 1) }
     let!(:level_2) { cards.second.levels.create!(user: user, status: 1, sort_order: 1) }
-    let!(:level_3) { cards.third.levels.create!(user: user, status: 0, sort_order: 1) }
+    let!(:level_3) { cards.third.levels.create!(user: user, status: 1, sort_order: 3) }
     let!(:level_5) { cards.fifth.levels.create!(user: user, status: 1, sort_order: 2) }
-    # cards.fourth without levels
+    # cards.fourth and sixth without levels
 
     before { sign_in user }
 
-    it "when the user learns cards with level 0" do
+    it "when the user learns new cards with level 0" do
       visit new_cardset_card_level_path(cardset, cards.fourth)
       should have_content('question 4')
       # click_button 'Show answer' not necessary because all html is shown for capybara
       click_on 'false'
-      should have_content('question 3')
+      should have_content('question 6')
       click_button 'correct'
-      should have_content('question 4')
+      should have_content('Overview')
     end
 
     it "when the user learns cards with level 1" do
@@ -36,6 +36,10 @@ describe "LearningPages" do
       should have_content('question 2')
       click_button 'false'
       should have_content('question 5')
+      click_button 'correct'
+      should have_content('question 3')
+      click_button 'correct'
+      should have_content('question 2')
       click_button 'correct'
       should have_content('Overview')
     end
